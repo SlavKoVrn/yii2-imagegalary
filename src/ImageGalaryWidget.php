160734db06fbb392d7d52ddbcdfbd475ca412e2c
SlavKoVrn <slavko.chita@gmail.com>
2019-02-10 01:56:23 +0300

    imagegalary with lightbox

--- src/ImageGalaryWidget.php
+++ src/ImageGalaryWidget.php
@@ -9,23 +9,15 @@ class ImageGalaryWidget extends Widget {
     
     public $id = 'imagegalary';
     public $class = 'imagegalary';
-    public $image_width = 0;
-    public $image_height = 0;
     public $thumb_width = 0;
     public $thumb_height = 0;
     public $images = [];
     public $items = 3;
     public $css = '';
-    private $div_width = 0;
-    private $div_height = 0;
     private $left='';
     private $right='';
-    private $loader='';
 
     public function init() {
-
-        $this->div_width = $this->image_width+50;
-        $this->div_height = $this->image_height+$this->thumb_height+20;
 
         parent::init();
 
@@ -40,10 +32,6 @@ class ImageGalaryWidget extends Widget {
         return $this->render('imagegalary',[
             'id' =>$this->id,
             'class' =>$this->class,
-            'div_width' =>$this->div_width,
-            'div_height' =>$this->div_height,
-            'image_width' =>$this->image_width,
-            'image_height' =>$this->image_height,
             'thumb_width' =>$this->thumb_width,
             'thumb_height' =>$this->thumb_height,
             'left' =>$this->left,
@@ -58,21 +46,24 @@ class ImageGalaryWidget extends Widget {
 
         $this->left=$path[1].'/images/left.png';
         $this->right=$path[1].'/images/right.png';
-        $this->loader=$path[1].'/img/loader.gif';
 
         $this->getView()->registerCssFile($path[1] . '/css/owl.carousel.css');
         $this->getView()->registerCssFile($path[1] . '/css/owl.theme.css');
         $this->getView()->registerCssFile($path[1] . '/css/imagegalary.css');
-        $this->getView()->registerCssFile($path[1] . '/css/prettyPhoto.css');
+        $this->getView()->registerCssFile($path[1] . '/css/lightbox.css');
         $this->getView()->registerCss('.'.$this->class.'{'.$this->css.'}');
 
 
-        $this->getView()->registerJsFile($path[1] . '/js/jquery.js');
-        $this->getView()->registerJsFile($path[1] . '/js/jquery.prettyPhoto.min.js');
-        $this->getView()->registerJsFile($path[1] . '/js/jquery.prettyPhoto.init.min.js');
-
         $this->getView()->registerJsFile(
             $path[1] . '/js/owl.carousel.js',
+            [
+                'position' => \yii\web\View::POS_END,
+                'depends'  => ['\yii\web\JqueryAsset'],
+            ]
+        );
+
+        $this->getView()->registerJsFile(
+            $path[1] . '/js/lightbox.js',
             [
                 'position' => \yii\web\View::POS_END,
                 'depends'  => ['\yii\web\JqueryAsset'],
--- src/views/imagegalary.php
+++ src/views/imagegalary.php
@@ -1,4 +1,4 @@
-<div class="row <?= $class ?>" style="width:<?= $div_width ?>px;height:<?= $div_height ?>px;text-align:center;">
+<div class="row <?= $class ?>" >
     <div id="<?= $id ?>-main-image" class="col-xs-12" style="text-align:center;margin:7px">
         <?php
 		$first=true;
@@ -6,16 +6,19 @@
 			if ($first){
 				$first=false;
 				$vid='';
-			}else
+                $image_id = 'id="main-image"';
+			}else{
 				$vid='style="display:none"';
+                $image_id = '';
+            }
         ?>
-    		<a <?= $vid; ?> href="<?= $img['src'] ?>" itemprop="image" class="zoom" title="<?= $img['title'] ?>" data-rel="prettyPhoto[<?= $id ?>]">
-                <img src="<?= $img['src'] ?>" title="<?= $img['title'] ?>" style="width:<?= $image_width ?>px;height:<?= $image_height ?>px;" />
+    		<a data-clicked="1" <?= $vid; ?> href="<?= $img['src'] ?>"" data-lightbox="<?= $id ?>" data-title="<?= $img['title'] ?>" >
+                <img <?= $image_id ?> src="<?= $img['src'] ?>" alt="<?= $img['title'] ?>" style="width:100%;max-height:300px;" />
             </a>
         <?php } ?>
     </div>
     <div class="col-xs-1" style="padding:0;margin:0;">
-        <img style="width:22px;cursor:pointer;" id="<?= $id ?>-left" src="<?= $left ?>" />
+        <img style="width:22px;cursor:pointer;" id="<?= $id ?>-left" src="<?= $left ?>" alt="Left" />
     </div>
     <div id="<?= $id ?>" class="col-xs-10">
         <?php foreach ($images as $img) { ?>
@@ -23,6 +26,6 @@
         <?php } ?>
     </div>
     <div class="col-xs-1" style="padding:0;margin:0;">
-        <img style="width:22px;cursor:pointer;padding:0;margin:0;" id="<?= $id ?>-right" src="<?= $right ?>" />
+        <img style="width:22px;cursor:pointer;padding:0;margin:0;" id="<?= $id ?>-right" src="<?= $right ?>" alt="Right" />
     </div>
 </div>
